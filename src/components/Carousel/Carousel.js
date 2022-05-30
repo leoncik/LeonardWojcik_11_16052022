690247c92796773ee2ec9575adb26faede070777
Léonard Wojcik <leonardw@tutanota.com>
2022-05-30 14:05:44 +0200

    feat: improve location carousel - Enable looping for next and previous images. - Improve responsivity. - Improve styling.

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -4,54 +4,71 @@ import classes from './Carousel.module.css';
 import NextImage from './NextImage';
 import PreviousImage from './PreviousImage';
 
-let counter = 1;
+let counter = 0;
 
 const Carousel = ({ images }) => {
 	const totalOfImages = images.length;
 	let shifting = 0;
 
 	const carousel = useRef();
-	// ! Not working, use mock value temporarily
-	// let carouselItemWidth = carousel.getBoundingClientRect.width;
-	let carouselItemWidth = 1166;
+	let carouselItemWidth = 0;
 
-	// const carouselTrack = document.querySelector('.carousel-track');
 	let carouselTrack = useRef();
 
 	const [imageNumber, setImageNumber] = useState(1);
 
+	// Todo : wait end of animation before clicking again. Maybe with a condition like : "animationPlayState == 'paused'" ?
+	// Todo : fix "setImageNumber" value when going backward.
+	// Todo : reset values when leaving page.
+
+	// Todo : improve responsivity (may require HTML changes).
+	// https://stackoverflow.com/questions/37891839/keep-one-element-centered-between-two-elements-of-different-widths-in-flexbox
+
 	const handleNextImage = () => {
+		carouselItemWidth = carousel.current.getBoundingClientRect().width;
+		counter++;
 		setImageNumber(imageNumber + 1);
 		shifting = counter * carouselItemWidth;
-		carouselTrack.style.transition = '1s linear';
-		carouselTrack.style.transform = `translateX(-${shifting}px)`;
-		counter++;
-		if (counter > totalOfImages + 1) {
-			counter = 1;
-			carouselTrack.style.transition = 'unset';
-			carouselTrack.style.transform = `translateX(0)`;
+		carouselTrack.current.style.transition = '800ms linear';
+		carouselTrack.current.style.transform = `translateX(-${shifting}px)`;
+		if (counter === totalOfImages) {
 			setImageNumber(1);
-			// handleNextImage();
-			// Could be fixed with a setTimeout of 1ms
-			// ! Uncaught TypeError: carouselTrack is null.
-			// setTimeout(handleNextImage, 1);
+		}
+		if (counter > totalOfImages) {
+			carouselItemWidth = carousel.current.getBoundingClientRect().width;
+			counter = 0;
+			carouselTrack.current.style.transition = 'unset';
+			carouselTrack.current.style.transform = `translateX(0)`;
+			// Add setTimeout to prevent animation conflict
+			setTimeout(handleNextImage, 1);
 		}
 	};
 
 	const handlePreviousImage = () => {
-		setImageNumber(imageNumber - 1);
-		shifting = counter * carouselItemWidth;
-		carouselTrack.style.transition = '1s linear';
-		carouselTrack.style.transform = `translateX(${shifting}px)`;
-		counter--;
+		if (counter >= 0) {
+			carouselItemWidth = carousel.current.getBoundingClientRect().width;
+			counter--;
+			setImageNumber(imageNumber - 1);
+			console.log(imageNumber);
+			shifting = -counter * carouselItemWidth;
+			carouselTrack.current.style.transition = '800ms linear';
+			carouselTrack.current.style.transform = `translateX(${shifting}px)`;
+		}
+		if (counter < 0) {
+			carouselItemWidth = carousel.current.getBoundingClientRect().width;
+			counter = totalOfImages;
+			setImageNumber(totalOfImages);
+			shifting = -counter * carouselItemWidth;
+			carouselTrack.current.style.transition = 'unset';
+			carouselTrack.current.style.transform = `translateX(${shifting}px)`;
+			// Add setTimeout to prevent animation conflict
+			setTimeout(handlePreviousImage, 1);
+		}
 	};
 
 	return (
 		<div ref={carousel} className={classes['location-carousel']}>
-			<div
-				ref={(ref) => (carouselTrack = ref)}
-				className={classes['carousel-track']}
-			>
+			<div ref={carouselTrack} className={classes['carousel-track']}>
 				{images.map((image, index) => (
 					<div key={index} className={classes['carousel-item']}>
 						<img src={image} />
