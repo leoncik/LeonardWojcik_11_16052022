3fc404d42461ecf2f6542a96a8911675fb71aefb
Léonard Wojcik <leonardw@tutanota.com>
2022-06-06 18:20:13 +0200

    refactor: subdivide carousel tasks in functions

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -4,6 +4,7 @@ import classes from './Carousel.module.css';
 import NextImage from './NextImage';
 import PreviousImage from './PreviousImage';
 
+// ! Counter does not reset after page change
 let counter = 0;
 const Carousel = ({ images }) => {
 	const totalOfImages = images.length;
@@ -19,91 +20,145 @@ const Carousel = ({ images }) => {
 	const [animate, setAnimate] = useState(true);
 	const animationDuration = 800;
 
+	const checkImageNumber = (direction) => {
+		switch (direction) {
+			case 'toNext': {
+				if (imageNumber === totalOfImages) {
+					setImageNumber(1);
+					counter = 0;
+				} else {
+					setImageNumber(imageNumber + 1);
+					counter++;
+				}
+
+				break;
+			}
+
+			case 'toPrevious': {
+				if (imageNumber === 1) {
+					setImageNumber(totalOfImages);
+					counter = totalOfImages - 1;
+				} else {
+					setImageNumber(imageNumber - 1);
+					counter--;
+				}
+				break;
+			}
+
+			default:
+				break;
+		}
+	};
+
+	const moveImages = (direction) => {
+		switch (direction) {
+			case 'toNext': {
+				// Select all carousel items
+				carouselItems = carouselTrack.current.children;
+				for (let i = carouselItems.length; i--; ) {
+					carouselItems[i].style.left = `${(i - 2) * 100}%`;
+					carouselItems[
+						i
+					].style.transition = `${animationDuration}ms linear left`;
+					// Wait end of animation before enabling click again.
+					setTimeout(() => {
+						setAnimate(true);
+					}, animationDuration);
+				}
+
+				break;
+			}
+
+			case 'toPrevious': {
+				// Select all carousel items
+				carouselItems = carouselTrack.current.children;
+				for (let i = carouselItems.length; i--; ) {
+					carouselItems[i].style.left = `${i * 100}%`;
+					carouselItems[
+						i
+					].style.transition = `${animationDuration}ms linear left`;
+					// Wait end of animation before enabling click again.
+					setTimeout(() => {
+						setAnimate(true);
+					}, animationDuration);
+				}
+				break;
+			}
+
+			default:
+				break;
+		}
+	};
+
+	const rearrangeImages = (direction) => {
+		switch (direction) {
+			case 'toNext': {
+				// Remove first child of carouselTrack
+				carouselTrack.current.removeChild(carouselTrack.current.firstChild);
+				// Create a new carousel item at the end of carouselTrack
+				const newCarouselItem = document.createElement('li');
+				newCarouselItem.className = classes['carousel-item'];
+				const newCarouselItemImage = document.createElement('img');
+				// Set src according to imageNumber
+				if (counter === totalOfImages - 1) {
+					newCarouselItemImage.src = images[0];
+				} else {
+					newCarouselItemImage.src = images[counter + 1];
+				}
+				newCarouselItem.appendChild(newCarouselItemImage);
+				newCarouselItem.style.left = `${(carouselItems.length - 1) * 100}%`;
+				newCarouselItem.style.transition = `${animationDuration}ms linear left`;
+				carouselTrack.current.appendChild(newCarouselItem);
+
+				break;
+			}
+
+			case 'toPrevious': {
+				// Remove last child of carouselTrack
+				carouselTrack.current.removeChild(carouselTrack.current.lastChild);
+				// Create a new carousel item at the beginning of carouselTrack
+				const newCarouselItem = document.createElement('li');
+				newCarouselItem.className = classes['carousel-item'];
+				const newCarouselItemImage = document.createElement('img');
+				// Set src according to imageNumber
+				if (counter === -1) {
+					newCarouselItemImage.src = images[totalOfImages - 2];
+				} else if (counter === 0) {
+					newCarouselItemImage.src = images[totalOfImages - 1];
+				} else {
+					newCarouselItemImage.src = images[counter - 1];
+				}
+				newCarouselItem.appendChild(newCarouselItemImage);
+				newCarouselItem.style.left = `${-1 * 100}%`;
+				newCarouselItem.style.transition = `${animationDuration}ms linear left`;
+				carouselTrack.current.insertBefore(
+					newCarouselItem,
+					carouselTrack.current.firstChild,
+				);
+				break;
+			}
+
+			default:
+				break;
+		}
+	};
+
 	const handleNextImage = () => {
+		console.log(counter);
 		if (animate === true) {
 			setAnimate(false);
-			if (imageNumber === totalOfImages) {
-				setImageNumber(1);
-			} else {
-				setImageNumber(imageNumber + 1);
-			}
-			counter++;
-			// Select all carousel items
-			carouselItems = carouselTrack.current.children;
-			for (let i = carouselItems.length; i--; ) {
-				carouselItems[i].style.left = `${(i - 2) * 100}%`;
-				carouselItems[
-					i
-				].style.transition = `${animationDuration}ms linear left`;
-				// Wait end of animation before enabling click again.
-				setTimeout(() => {
-					setAnimate(true);
-				}, animationDuration);
-			}
-			// Remove first child of carouselTrack
-			carouselTrack.current.removeChild(carouselTrack.current.firstChild);
-			// Create a new carousel item at the end of carouselTrack
-			const newCarouselItem = document.createElement('li');
-			newCarouselItem.className = classes['carousel-item'];
-			const newCarouselItemImage = document.createElement('img');
-			// Set src according to imageNumber
-			if (counter === totalOfImages - 1) {
-				newCarouselItemImage.src = images[0];
-				counter = -1;
-			} else {
-				newCarouselItemImage.src = images[counter + 1];
-			}
-			newCarouselItem.appendChild(newCarouselItemImage);
-			newCarouselItem.style.left = `${(carouselItems.length - 1) * 100}%`;
-			newCarouselItem.style.transition = `${animationDuration}ms linear left`;
-			carouselTrack.current.appendChild(newCarouselItem);
+			checkImageNumber('toNext');
+			moveImages('toNext');
+			rearrangeImages('toNext');
 		}
 	};
 
 	const handlePreviousImage = () => {
 		if (animate === true) {
-			console.log(counter);
 			setAnimate(false);
-			if (imageNumber === 1) {
-				setImageNumber(totalOfImages);
-			} else {
-				setImageNumber(imageNumber - 1);
-			}
-			counter--;
-			// Select all carousel items
-			carouselItems = carouselTrack.current.children;
-			for (let i = carouselItems.length; i--; ) {
-				carouselItems[i].style.left = `${i * 100}%`;
-				carouselItems[
-					i
-				].style.transition = `${animationDuration}ms linear left`;
-				// Wait end of animation before enabling click again.
-				setTimeout(() => {
-					setAnimate(true);
-				}, animationDuration);
-			}
-			// Remove last child of carouselTrack
-			carouselTrack.current.removeChild(carouselTrack.current.lastChild);
-			// Create a new carousel item at the beginning of carouselTrack
-			const newCarouselItem = document.createElement('li');
-			newCarouselItem.className = classes['carousel-item'];
-			const newCarouselItemImage = document.createElement('img');
-			// Set src according to imageNumber
-			if (counter === -1) {
-				newCarouselItemImage.src = images[totalOfImages - 2];
-				counter = totalOfImages - 1;
-			} else if (counter === 0) {
-				newCarouselItemImage.src = images[totalOfImages - 1];
-			} else {
-				newCarouselItemImage.src = images[counter - 1];
-			}
-			newCarouselItem.appendChild(newCarouselItemImage);
-			newCarouselItem.style.left = `${-1 * 100}%`;
-			newCarouselItem.style.transition = `${animationDuration}ms linear left`;
-			carouselTrack.current.insertBefore(
-				newCarouselItem,
-				carouselTrack.current.firstChild,
-			);
+			checkImageNumber('toPrevious');
+			moveImages('toPrevious');
+			rearrangeImages('toPrevious');
 		}
 	};
 
