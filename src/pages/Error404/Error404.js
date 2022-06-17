83865e7510742f33982949a20bc9da416e4435e7
Léonard Wojcik <leonardw@tutanota.com>
2022-06-17 09:07:13 +0200

    styling adjustments - Remove slide number on mobile. - Adjust size and position of various elements.

--- src/pages/Error404/Error404.js
+++ src/pages/Error404/Error404.js
@@ -3,11 +3,11 @@ import classes from './Error404.module.css';
 
 const Error404 = () => {
 	return (
-		<div className={classes['error404-container']}>
+		<main className={classes['error404-container']}>
 			<h1>404</h1>
 			<p>{"Oups! La page que vous demandez n'existe pas."} </p>
 			<Link to="/">{'Retourner sur la page d’accueil'}</Link>
-		</div>
+		</main>
 	);
 };
 
